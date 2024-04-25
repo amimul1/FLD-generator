229bbe05d11bd454094246261cdcd3eb04e2b884
MorishT <magi.magilmagi@gmail.com>
2024-04-25 18:27:06 +0900

    [exp] ./outputs/00.create_corpus/2024-03-29 with 2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.1 and 2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.03

--- launchers/experimental_settings.py
+++ launchers/experimental_settings.py
@@ -9274,6 +9274,124 @@ _DATASET_SETTINGS = {
     },
 
 
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.03': {
+
+        'argument_configs': [
+            './configs/arguments/predicate/specified/axioms/',
+            './configs/arguments/propositional/axioms/',
+
+            './configs/arguments/predicate/specified/references/',
+            './configs/arguments/propositional/references/',
+            './configs/arguments/predicate/quantified/references/',
+        ],
+        'quantifier_axioms': [
+            'universal_quantifier_elim',
+            'universal_quantifier_intro',
+            'existential_quantifier_intro',
+            'existential_quantifier_elim',
+        ],
+        'quantifier_axiom_arguments_weight': 0.2,
+        'complex_formula_arguments_weight': 0.5,
+        'propositional_arguments_factor': 1.0,
+        'theorem_arguments_factor': 0.03,
+
+
+        'depth_range': (1, 3),
+        'depth_distrib': 'flat',
+        'branch_extensions_range': (0, 5),
+
+
+        'distractor': 'mixture(negative_tree_double.simplified_formula.various_form)',
+        'distractors_range': (0, 20),
+        'translation_distractors_range': (0, 0),
+        'use_collapsed_translation_nodes_for_unknown_tree': False,
+
+
+        'translation_configs': ['thing'],
+        'translation_volume_to_weight': 'log10',
+        'translation_adj_verb_noun_ratio': '1-1-1',
+
+
+        'split_wise_settings': {
+            'train': {
+                # 'sample_all_stances_per_logic': True,
+                # 'context_shuffles_per_instance': 3,
+                # 'translation_variants_per_logic': 3,
+            },
+            'valid': {
+            },
+            'test': {
+            },
+        },
+        'split_sizes': {
+            'test': 1000,
+            # 'valid': 1000,
+            'train': 100000,
+        },
+
+    },
+
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.1': {
+
+        'argument_configs': [
+            './configs/arguments/predicate/specified/axioms/',
+            './configs/arguments/propositional/axioms/',
+
+            './configs/arguments/predicate/specified/references/',
+            './configs/arguments/propositional/references/',
+            './configs/arguments/predicate/quantified/references/',
+        ],
+        'quantifier_axioms': [
+            'universal_quantifier_elim',
+            'universal_quantifier_intro',
+            'existential_quantifier_intro',
+            'existential_quantifier_elim',
+        ],
+        'quantifier_axiom_arguments_weight': 0.2,
+        'complex_formula_arguments_weight': 0.5,
+        'propositional_arguments_factor': 1.0,
+        'theorem_arguments_factor': 0.1,
+
+
+        'depth_range': (1, 3),
+        'depth_distrib': 'flat',
+        'branch_extensions_range': (0, 5),
+
+
+        'distractor': 'mixture(negative_tree_double.simplified_formula.various_form)',
+        'distractors_range': (0, 20),
+        'translation_distractors_range': (0, 0),
+        'use_collapsed_translation_nodes_for_unknown_tree': False,
+
+
+        'translation_configs': ['thing'],
+        'translation_volume_to_weight': 'log10',
+        'translation_adj_verb_noun_ratio': '1-1-1',
+
+
+        'split_wise_settings': {
+            'train': {
+                # 'sample_all_stances_per_logic': True,
+                # 'context_shuffles_per_instance': 3,
+                # 'translation_variants_per_logic': 3,
+            },
+            'valid': {
+            },
+            'test': {
+            },
+        },
+        'split_sizes': {
+            'test': 1000,
+            # 'valid': 1000,
+            'train': 100000,
+        },
+
+    },
+
+
+
     '2024-03-29.JSAI_best.no_aug.trnsl-v2': {
 
         'argument_configs': [
@@ -9329,6 +9447,123 @@ _DATASET_SETTINGS = {
         },
 
     },
+
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.03': {
+
+        'argument_configs': [
+            './configs/arguments/predicate/specified/axioms/',
+            './configs/arguments/propositional/axioms/',
+
+            './configs/arguments/predicate/specified/references/',
+            './configs/arguments/propositional/references/',
+            './configs/arguments/predicate/quantified/references/',
+        ],
+        'quantifier_axioms': [
+            'universal_quantifier_elim',
+            'universal_quantifier_intro',
+            'existential_quantifier_intro',
+            'existential_quantifier_elim',
+        ],
+        'quantifier_axiom_arguments_weight': 0.2,
+        'complex_formula_arguments_weight': 0.5,
+        'propositional_arguments_factor': 1.0,
+        'theorem_arguments_factor': 0.03,
+
+
+        'depth_range': (1, 3),
+        'depth_distrib': 'flat',
+        'branch_extensions_range': (0, 5),
+
+
+        'distractor': 'mixture(negative_tree_double.simplified_formula.various_form)',
+        'distractors_range': (0, 20),
+        'translation_distractors_range': (0, 0),
+        'use_collapsed_translation_nodes_for_unknown_tree': False,
+
+
+        'translation_configs': ['thing_person.v2'],
+        'translation_volume_to_weight': 'log10',
+        'translation_adj_verb_noun_ratio': '1-1-1',
+
+
+        'split_wise_settings': {
+            'train': {
+                # 'sample_all_stances_per_logic': True,
+                # 'context_shuffles_per_instance': 3,
+                # 'translation_variants_per_logic': 3,
+            },
+            'valid': {
+            },
+            'test': {
+            },
+        },
+        'split_sizes': {
+            'test': 1000,
+            # 'valid': 1000,
+            'train': 100000,
+        },
+
+    },
+
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.1': {
+
+        'argument_configs': [
+            './configs/arguments/predicate/specified/axioms/',
+            './configs/arguments/propositional/axioms/',
+
+            './configs/arguments/predicate/specified/references/',
+            './configs/arguments/propositional/references/',
+            './configs/arguments/predicate/quantified/references/',
+        ],
+        'quantifier_axioms': [
+            'universal_quantifier_elim',
+            'universal_quantifier_intro',
+            'existential_quantifier_intro',
+            'existential_quantifier_elim',
+        ],
+        'quantifier_axiom_arguments_weight': 0.2,
+        'complex_formula_arguments_weight': 0.5,
+        'propositional_arguments_factor': 1.0,
+        'theorem_arguments_factor': 0.1,
+
+
+        'depth_range': (1, 3),
+        'depth_distrib': 'flat',
+        'branch_extensions_range': (0, 5),
+
+
+        'distractor': 'mixture(negative_tree_double.simplified_formula.various_form)',
+        'distractors_range': (0, 20),
+        'translation_distractors_range': (0, 0),
+        'use_collapsed_translation_nodes_for_unknown_tree': False,
+
+
+        'translation_configs': ['thing_person.v2'],
+        'translation_volume_to_weight': 'log10',
+        'translation_adj_verb_noun_ratio': '1-1-1',
+
+
+        'split_wise_settings': {
+            'train': {
+                # 'sample_all_stances_per_logic': True,
+                # 'context_shuffles_per_instance': 3,
+                # 'translation_variants_per_logic': 3,
+            },
+            'valid': {
+            },
+            'test': {
+            },
+        },
+        'split_sizes': {
+            'test': 1000,
+            # 'valid': 1000,
+            'train': 100000,
+        },
+
+    },
+
 
 
     '2024-03-29.JSAI_best.no_aug': {
@@ -10214,17 +10449,27 @@ _DATASET_NAME_TO_DEFAULT = {
     '2024-03-29.JSAI_best.no_aug.dstrctr-10': '20231018.thing_person_config_translation',
     '2024-03-29.JSAI_best.no_aug.cmplx-0.25': '20231018.thing_person_config_translation',
     '2024-03-29.JSAI_best.no_aug.quant-0.5': '20231018.thing_person_config_translation',
-    '2024-03-29.JSAI_best.no_aug.trnsl-v2': '20231018.thing_person_config_translation',
-    '2024-03-29.JSAI_best.no_aug.trnsl-thing': '20231018.thing_person_config_translation',
 
+
+
+    # ------------------------------- production --------------------------------
+    '2024-03-29.FLD_v2.D8': '20230626.many_bugs_fixed',
 
 
     '2024-03-29.FLD_v2': '20230626.many_bugs_fixed',
-    '2024-03-29.FLD_v2.D8': '20230626.many_bugs_fixed',
     '2024-03-29.FLD_v2.theorems-0.03': '20230626.many_bugs_fixed',
     '2024-03-29.FLD_v2.theorems-0.3.fix': '20230626.many_bugs_fixed',
     '2024-03-29.FLD_v2.theorems-0.1.fix': '20230626.many_bugs_fixed',
     '2024-03-29.FLD_v2.theorems-0.03.fix': '20230626.many_bugs_fixed',
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-thing': '20231018.thing_person_config_translation',
+    '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.1': '20231018.thing_person_config_translation',
+    '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.03': '20231018.thing_person_config_translation',
+
+    '2024-03-29.JSAI_best.no_aug.trnsl-v2': '20231018.thing_person_config_translation',
+    '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.1': '20231018.thing_person_config_translation',
+    '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.03': '20231018.thing_person_config_translation',
+
 }
 
 
--- launchers/run_create_corpus.py
+++ launchers/run_create_corpus.py
@@ -301,59 +301,86 @@ def main():
         # '2024-03-29.JSAI_best.D8',
         # '2024-03-29.JSAI_best.theorems',
 
-
         # '2024-03-29.JSAI_best.no_aug',
         # '2024-03-29.JSAI_best.D8.no_aug',
+
         # '2024-03-29.JSAI_best.theorems.no_aug',
         # '2024-03-29.JSAI_best.theorems-0.1.no_aug',
         # '2024-03-29.JSAI_best.theorems-0.03.no_aug',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-v2',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing',
-
 
         # '2024-03-29.JSAI_best.no_aug.dstrctr-10',
         # '2024-03-29.JSAI_best.no_aug.cmplx-0.25',
         # '2024-03-29.JSAI_best.no_aug.quant-0.5',
 
+
+
+        # ------------------------------- production --------------------------------
+
         # '2024-03-29.FLD_v2',
         # '2024-03-29.FLD_v2.D8',
         # '2024-03-29.FLD_v2.theorems-0.03',
-        # '2024-03-29.FLD_v2.theorems-0.3.fix'
-        '2024-03-29.FLD_v2.theorems-0.1.fix'
-        '2024-03-29.FLD_v2.theorems-0.03.fix'
+        # '2024-03-29.FLD_v2.theorems-0.3.fix',
+        # '2024-03-29.FLD_v2.theorems-0.1.fix',
+        # '2024-03-29.FLD_v2.theorems-0.03.fix',
+
+
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.1',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.03',
+
+        # '2024-03-29.JSAI_best.no_aug.trnsl-v2',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.1',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.03',
+
 
     ]
+
+
+    # wait_before_gather = True
+    wait_before_gather = False   # avoid using too much jobs in parallel, which may lead to os error (BrokenPipeError) in HAIC
+
+
+    only_gather = False
+    # only_gather = True
+
+
+    # job_engine = SubprocessEngine()
+    # job_engine = QsubEngine('ABCI', 'rt_C.small')
+
+    job_engine = QsubEngine('haic', 'xcs_s.small')
+    # job_engine = QsubEngine('haic', 'xcl_s.small')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # ---------------------------- fixed settings --------------------------
+    dry_run = False
+    # dry_run = True
+
+    # skip_if_exists = False
+    skip_if_exists = True
 
     num_jobs_for_datasets = 2
     num_jobs_per_dataset = 300
     # num_jobs_per_dataset = 900
 
+
     # for the case some jobs hangs
     timeout_per_job = 3600 * 3
 
-    # wait_before_gather = True
-    wait_before_gather = False   # avoid using too much jobs in parallel, which may lead to os error (BrokenPipeError) in HAIC
+    delete_logs_when_done = True
 
-    only_gather = False
-    # only_gather = True
-
-    # skip_if_exists = False
-    skip_if_exists = True
-
-
-    # job_engine = SubprocessEngine()
-
-    # job_engine = QsubEngine('ABCI', 'rt_C.small')
-
-    # job_engine = QsubEngine('haic', 'xcs_s.small')
-    job_engine = QsubEngine('haic', 'xcl_s.small')  # USE THIS TOO
-
-    # job_engine = QsubEngine('haic', 'xcs_s.tiny')
-
-    dry_run = False
-    # dry_run = True
-
-    # ---------------------------- fixed settings --------------------------
     if job_engine.resource == 'rt_C.small':
         num_workers_per_job = 5
 
@@ -368,8 +395,6 @@ def main():
 
     # -- large value can save ABCI points because it avoids that the data loading becomes the bottleneck.
     min_dataset_size_per_job = 30 * num_workers_per_job
-
-    delete_logs_when_done = True
 
     if isinstance(job_engine, QsubEngine) and job_engine.region == 'ABCI' and \
             num_jobs_for_datasets * num_jobs_per_dataset > 180:
