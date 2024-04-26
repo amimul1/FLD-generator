355cd7b9c296f31548a888ab0a2bc1bba083aa64
MorishT <magi.magilmagi@gmail.com>
2024-04-26 17:03:37 +0900

    [exp] ./outputs/00.create_corpus/2024-03-29 with 2024-03-29.JSAI_best.no_aug.theorems-0.3

--- launchers/run_create_corpus.py
+++ launchers/run_create_corpus.py
@@ -323,18 +323,18 @@ def main():
         # '2024-03-29.FLD_v2.theorems-0.03.fix',
 
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing',
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.3',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.3',
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.1',
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing.theorems-0.03',
 
         # '2024-03-29.JSAI_best.no_aug.trnsl-v2',
-        '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.3',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.3',
         # '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.1',
         # '2024-03-29.JSAI_best.no_aug.trnsl-v2.theorems-0.03',
 
 
         # '2024-03-29.JSAI_best.no_aug',
-        # '2024-03-29.JSAI_best.no_aug.theorems-0.3',
+        '2024-03-29.JSAI_best.no_aug.theorems-0.3',
         # '2024-03-29.JSAI_best.no_aug.theorems-0.1',
         # '2024-03-29.JSAI_best.no_aug.theorems-0.03',
     ]
@@ -344,15 +344,15 @@ def main():
     wait_before_gather = False   # avoid using too much jobs in parallel, which may lead to os error (BrokenPipeError) in HAIC
 
 
-    # only_gather = False
-    only_gather = True
+    only_gather = False
+    # only_gather = True
 
 
     # job_engine = SubprocessEngine()
     # job_engine = QsubEngine('ABCI', 'rt_C.small')
 
-    # job_engine = QsubEngine('haic', 'xcs_s.small')
-    job_engine = QsubEngine('haic', 'xcl_s.small')
+    job_engine = QsubEngine('haic', 'xcs_s.small')
+    # job_engine = QsubEngine('haic', 'xcl_s.small')
 
 
 
